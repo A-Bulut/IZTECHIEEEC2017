4759ca9be2df00b9f51620e120e2ab74157a7402
A-Bulut <34038929+A-Bulut@users.noreply.github.com>
2017-11-27 22:10:33 +0300

    Add files via upload

--- week1/w1q1.cpp
+++ week1/w1q1.cpp
@@ -2,16 +2,18 @@
 
 
 int main(){
-	int i,j;
-	for(i=0;i<15;i++){
-		
-		for(j=0;j<i;j++){
-		
-		printf("\t");
-		}
-		printf("Hello World!\n");
+	int i;
+	char n[]={'E','L','L','O','W','O','R','L','D','!','\0'};
+	
+	printf("H ");
+	
+	for(i=0;i<=9;i++){
+		printf("%c ",n[i]);
 	}	
 	
+	for(i=0;i<=9;i++){
+		printf("\n%c",n[i]);
+	}
 	
 	return 0;
 }
